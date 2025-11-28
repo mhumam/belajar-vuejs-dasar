f3aee8223fb9be29dfac2a05458c146f1e18f34c
Muhamad Humam <muhamadhumamm17@gmail.com>
2025-11-28 14:27:55 +0700

    Add multiple attribute binding examples to HelloVue component

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -10,11 +10,17 @@
 
     const buttonDisabled = true;
     const buttonEnabled = false;
+
+    const helloMultipleAttributed = "<h1>Belajar template multiple attributes</h1>";
+    const dataMultipleAttribute = {
+        id: "hello",
+        class: "hello"
+    }
 </script>
 
 <template>
     <h1 class="hello">Hello Vue Component</h1>
-    <h1 class="hello">{{hello}} (dipanggil dengan mustache, dirender sebagai string)</h1>
+    <h1 class="hello">{{ hello }} (dipanggil dengan mustache, dirender sebagai string)</h1>
     <div v-html="vHtml"></div>
     
     <h1 :id v-bind:class="classHello">Memanggil class dengan directive class v-bind:class</h1>
@@ -22,6 +28,9 @@
 
     <button :disabled="buttonDisabled">Button disabled</button>
     <button :disabled="buttonEnabled">Button enabled</button>
+
+    <h1 v-bind="dataMultipleAttribute">{{ helloMultipleAttributed }}</h1>
+    <div :class="dataMultipleAttribute.class" v-html="helloMultipleAttributed"></div>
 </template>
 
 <style scoped>
