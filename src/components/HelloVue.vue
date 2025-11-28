31e72ea308a412e76d96b1ba49c07901e890dd99
Muhamad Humam <muhamadhumamm17@gmail.com>
2025-11-28 14:18:33 +0700

    Expand HelloVue component with additional examples of class and ID binding using v-bind directives

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -3,12 +3,20 @@
 
     const hello = "<b>Belajar template</b>";
     const vHtml = "<h1>Belajar template (dipanggil sebagai html dengan directive html v-)</h1>";
+
+    /* Directive class | v-bind:class, :class, :id */
+    const classHello = "hello";
+    const id = "id-name";
 </script>
 
 <template>
     <h1 class="hello">Hello Vue Component</h1>
     <h1 class="hello">{{hello}} (dipanggil dengan mustache, dirender sebagai string)</h1>
     <div v-html="vHtml"></div>
+
+    
+    <h1 :id v-bind:class="classHello">Memanggil class dengan directive class v-bind:class</h1>
+    <div :class="classHello" v-html="hello"></div>
 </template>
 
 <style scoped>
