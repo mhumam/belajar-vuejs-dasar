7460160323c1d305dd792ecc2414721ab642248a
Muhamad Humam <muhamadhumamm17@gmail.com>
2025-11-28 14:07:49 +0700

    Enhance HelloVue component by adding template rendering examples using mustache and v-html directives

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -1,9 +1,14 @@
 <script setup>
     console.log("Hello Vue Component");
+
+    const hello = "<b>Belajar template</b>";
+    const vHtml = "<h1>Belajar template (dipanggil sebagai html dengan directive html v-)</h1>";
 </script>
 
 <template>
     <h1 class="hello">Hello Vue Component</h1>
+    <h1 class="hello">{{hello}} (dipanggil dengan mustache, dirender sebagai string)</h1>
+    <div v-html="vHtml"></div>
 </template>
 
 <style scoped>
